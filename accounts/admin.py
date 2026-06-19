57770ae5a56277cb4c452ffb5d27a98fe0bf1b0b
SeanMugo <sean.mugo08@Gmail.com>
2026-06-19 13:31:59 +0300

    Add authentication URLs

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
+# accounts/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'wallet_balance', 'is_active')
+    list_filter = ('role', 'is_active')
+    search_fields = ('username', 'email', 'phone_number')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('role', 'phone_number', 'wallet_balance')
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
