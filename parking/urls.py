29fd7684e434b84a358af4566c34b37babf139a0
SeanMugo <sean.mugo08@Gmail.com>
2026-06-24 21:53:30 +0300

    Ensure AdminSlotViewSet is defined and imported

--- parking/urls.py
+++ parking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ParkingSlotViewSet, BookingViewSet
+from .views import ParkingSlotViewSet, BookingViewSet, AdminSlotViewSet
 
 router = DefaultRouter()
 router.register(r'slots', ParkingSlotViewSet, basename='slot')
