72ca650a92872b9bd5eb00f3243ec00da811620c
SeanMugo <sean.mugo08@Gmail.com>
2026-07-14 19:20:20 +0300

    fix: Registration page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import (
+    RegisterView,
     LoginView,
     LogoutView,
     ProfileView,
@@ -16,6 +17,7 @@ from .views import (
 
 urlpatterns = [
     # Authentication
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("profile/", ProfileView.as_view(), name="profile"),
--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import get_user_model
 
 from .serializers import (
+    RegisterSerializer,
     UserSerializer,
     UpdateProfileSerializer,
     AdminUpdateUserSerializer,
@@ -18,7 +19,33 @@ from .serializers import (
 
 User = get_user_model()
 
+# ==========================================
+# REGISTER
+# ==========================================
 
+class RegisterView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(
+                {
+                    "message": "Registration successful."
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 # ==========================================
 # LOGIN
 # ==========================================
